397b14684c11713ce9cc19e45159ddec0b170946
Yogender <104613019+YogenderJ@users.noreply.github.com>
2022-12-14 17:56:56 +0530

    Create aws-exports.js

---
+++ aws-exports.js
@@ -0,0 +1,5 @@
+import { config } from 'aws-sdk'const AWSConfig = {
+    accessKeyId: AKIASNTGCFE6EMAPVPPP,
+    secretAccessKey: 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,
+    region: us-west-2,
+}config.update(AWSConfig)
