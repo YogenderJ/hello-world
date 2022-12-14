090bf97882c15ea750cf06549ba7f7d2ceb72c9f
Yogender <104613019+YogenderJ@users.noreply.github.com>
2022-12-14 18:20:43 +0530

    Delete aws-exports.js

--- aws-exports.js
+++
@@ -1,5 +0,0 @@
-import { config } from 'aws-sdk'const AWSConfig = {
-    accessKeyId: AKIASNTGCFE6EMAPVPPP,
-    secretAccessKey: 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,
-    region: us-west-2,
-}config.update(AWSConfig)
